5d9b3b467a66ac235a98e407cba40f4c68a0c0ec
Matioma <Artiom.Matievschi@gmail.com>
2020-12-02 19:41:53 +0100

    Geting the html page

--- ServerJs/Server.js
+++ ServerJs/Server.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var path = require("path");
 // Create a new express app instance
 var app = express();
+var staticPath = path.join(__dirname, "../public");
+console.log(staticPath);
+app.use(express.static(staticPath));
 app.get("/", function (req, res) {
-    res.send("Hello World!");
+    res.sendFile(path.join(staticPath, "/html/index.html"));
 });
 app.listen(3000, function () {
     console.log("App is listening on port 3000!");
--- ServerTypeScript/Server.ts
+++ ServerTypeScript/Server.ts
@@ -1,10 +1,18 @@
 import express = require("express");
+import path = require("path");
+
 // Create a new express app instance
 const app: express.Application = express();
 
-app.get("/", function (req: any, res: any) {
-  res.send(`Hello World!`);
+const staticPath: string = path.join(__dirname, "../public");
+console.log(staticPath);
+
+app.use(express.static(staticPath));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(staticPath, "/html/index.html"));
 });
+
 app.listen(3000, function () {
   console.log(`App is listening on port 3000!`);
 });
