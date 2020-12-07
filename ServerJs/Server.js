41e8261c141152eb50dc0c1b2da0845f1818707e
Matioma <Artiom.Matievschi@gmail.com>
2020-12-07 10:32:34 +0100

    Receive data from the client form

--- ServerJs/Server.js
+++ ServerJs/Server.js
@@ -6,9 +6,13 @@ var app = express();
 console.log("Hello world!!!!");
 var staticPath = path.join(__dirname, "../public");
 app.use(express.static(staticPath));
+app.use(express.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
-    //res.send("400");
     res.sendFile(path.join(staticPath, "/html/index.html"));
+});
+app.post("/api/register", function (req, res) {
+    console.log(req.body);
+    res.redirect("/");
 });
 app.listen(3000, function () {
     console.log("App is listening on port 3000!");
--- ServerTypeScript/Server.ts
+++ ServerTypeScript/Server.ts
@@ -7,10 +7,15 @@ console.log("Hello world!!!!");
 const staticPath: string = path.join(__dirname, "../public");
 
 app.use(express.static(staticPath));
-app.get("/", function (req, res) {
-  //res.send("400");
 
+app.use(express.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
   res.sendFile(path.join(staticPath, "/html/index.html"));
+});
+
+app.post("/api/register", function (req, res) {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
